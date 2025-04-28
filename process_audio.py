00478e5db63de0a4f19a500f2d75c09af9febf0f
Noah Butler <102554523+noahbutler7@users.noreply.github.com>
2025-04-28 04:54:14 -0400

    Update process_audio.py

--- process_audio.py
+++ process_audio.py
@@ -75,7 +75,6 @@ def upload_file():
     return jsonify({"audio_files": audio_response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
